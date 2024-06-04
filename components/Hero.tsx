11d9d226604182283b7c12ab649239c35dae44a1
Albin Lamichhane <39218356+aln1234@users.noreply.github.com>
2024-06-04 15:57:08 +0300

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -57,7 +57,7 @@ const Hero = () => {
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Adrian, a Next.js Developer based in Croatia.
+            Hi! I&apos;m Albin, a Next.js Developer based in Finland.
           </p>
 
           <a href="#about">
